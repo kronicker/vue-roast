d4fbc5eef02a7bb54cc5af036ceba7cc93a12d68
Dario Vladovic <d.vladimyr@gmail.com>
2017-08-28 14:48:09 +0200

    Adding build script :tada:
    - introducing poi powered build system

---
+++ poi.config.js
@@ -0,0 +1,11 @@
+const { name } = require('./package.json');
+
+module.exports = {
+  entry: './src/index.js',
+  vendor: false,
+  html: false,
+  filename: {
+    js: `${name}.min.js`,
+    css: 'style.css'
+  }
+};
