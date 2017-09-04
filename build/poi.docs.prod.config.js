b1a0cdc254ac6994452f401e8961631b67ca135f
Toma Zelić <zelic.toma@gmail.com>
2017-09-04 16:58:46 +0200

    Update docs

--- build/poi.docs.prod.config.js
+++ build/poi.docs.prod.config.js
@@ -1,9 +1,5 @@
 module.exports = {
   entry: './src/docs/main.js',
-  webpack(config) {
-    config.output.publicPath = './';
-    return config;
-  },
   dist: './docs',
   html: {
     template: './src/docs/index.html'
